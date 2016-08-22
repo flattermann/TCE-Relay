44b39eead3c5f07f4a3885aa8d648991c637216c
Christian Erpelding <flattermann@users.noreply.github.com>
2016-08-22 12:59:54 +0200

    check_stars: Added option to update Stars.db with systems.json data

--- tcetools/Stars/check_stars.py
+++ tcetools/Stars/check_stars.py
@@ -4,6 +4,8 @@ import json
 import sqlite3
 import timeit
 
+updateStars = True
+
 # You MUST have 64bit python for this or you'll get a MemoryError
 systems = json.load(open('c:/temp/systems.json', encoding="iso8859-1"))
 connStars = sqlite3.connect("c:/tce/db/TCE_Stars.db")
@@ -28,6 +30,11 @@ def getSystem(id):
     except KeyError:
         return None
 
+def updateStar(starId, starName, x, y, z):
+    global connStars
+    c = connStars.cursor()
+    c.execute("UPDATE public_Stars SET StarName=?, X=?, Y=?, Z=? WHERE ID=?", (starName, x, y, z, starId))
+
 countNameDiff=0
 countMissing=0
 countCoordsDiff=0
@@ -45,13 +52,19 @@ for star in stars:
     different=False
     if star["StarName"] != system["name"].upper():
         countNameDiff += 1
-        print ("Different name for", starId, star["StarName"], system["name"].upper())        
+        print ("Different name for", starId, star["StarName"], system["name"].upper())
         different=True
     if star["X"] != system["x"] or star["Y"] != system["y"] or star["Z"] != system["z"] :
         countCoordsDiff += 1
         print ("Different coords for", starId, star["X"], system["x"], star["Y"], system["y"], star["Z"], system["z"])
         different=True
+    if updateStars and different:
+        print("    -> Updating star")
+        updateStar(starId, system["name"].upper(), system["x"], system["y"], system["z"])
 
+if updateStars:
+    connStars.commit()
+    
 print("Checked",len(stars),"stars against",len(systems),"systems")
 print("Not in systems.json", countMissing)
 print("Name difference", countNameDiff)
