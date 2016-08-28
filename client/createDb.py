a94a5bfe2c1f7a26c13b6560f063412f34c45b69
Christian Erpelding <flattermann@users.noreply.github.com>
2016-08-28 12:25:42 +0200

    Added x/y/z to DB

--- client/createDb.py
+++ client/createDb.py
@@ -16,16 +16,23 @@ print("There are",total,"stations")
 connTceRelayClient = sqlite3.connect(scriptDir+"/TCE-RelayClient.db")
 connTceRelayClient.row_factory = sqlite3.Row
 
-systemNamesCache = {}
+systemCache = {}
 
 def getSystemNameById(id):
+    system = getSystemById(id)
+    if system == None:
+        return None
+    else:
+        return system["name"]
+
+def getSystemById(id):
     global systems
-    global systemNamesCache
-    if len(systemNamesCache) == 0:
+    global systemCache
+    if len(systemCache) == 0:
         for system in systems:
-            systemNamesCache[system["id"]] = system["name"]
+            systemCache[system["id"]] = system
     try:
-        return systemNamesCache[id]
+        return systemCache[id]
     except KeyError:
         return None
         
@@ -36,7 +43,25 @@ c = connTceRelayClient.cursor()
 c.execute("SELECT name FROM sqlite_master WHERE type='table';")
 print(c.fetchall())
 
-c.execute("DELETE FROM stationIdMappings")
+c.execute("DROP TABLE stationIdMappings")
+c.execute("""CREATE TABLE 'stationIdMappings' (
+    'id'            INTEGER,
+    'stationId'     INTEGER NOT NULL,
+    'systemId'      INTEGER NOT NULL,
+    'stationName'   TEXT NOT NULL,
+    'systemName'    TEXT NOT NULL,
+    'distanceStar'  INTEGER,
+    'starX'         DOUBLE,
+    'starY'         DOUBLE,
+    'starZ'         DOUBLE,
+    'main'          INTEGER NOT NULL,
+    PRIMARY KEY(id)
+    )""")
+c.execute("CREATE INDEX idx_distanceStar on stationIdMappings (distanceStar)")
+c.execute("CREATE INDEX idx_main on stationIdMappings (main)")
+c.execute("CREATE INDEX idx_stationId on stationIdMappings (stationId)")
+c.execute("CREATE INDEX idx_stationName_systemName on stationIdMappings (stationName, systemName)")
+c.execute("CREATE INDEX idx_systemId on stationIdMappings (systemId)")
 
 #id = 0
 marketCount = 0
@@ -50,22 +75,22 @@ for station in stations:
     has_market=station["has_market"]
 
     if name != None and has_market:
-        systemName = getSystemNameById(system_id)
+        system = getSystemById(system_id)
 
         name = name.upper()
-        systemName = systemName.upper()
+        systemName = getSystemNameById(system_id).upper()
         distanceStar = station["distance_to_star"]
 #		print (count,"/",marketCount,name,id)
         
-        c.execute("INSERT INTO stationIdMappings (stationId, systemId, stationName, systemName, distanceStar, main) VALUES (?, ?, ?, ?, ?, ?)", 
-            (id, system_id, name, systemName, distanceStar, 1))
+        c.execute("INSERT INTO stationIdMappings (stationId, systemId, stationName, systemName, distanceStar, starX, starY, starZ, main) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+            (id, system_id, name, systemName, distanceStar, system["x"], system["y"], system["z"], 1))
 
         # Add systemNames without ' as well
         systemNameAscii = systemName.replace("'", "")
             
         if (systemNameAscii != systemName):
-            c.execute("INSERT INTO stationIdMappings (stationId, systemId, stationName, systemName, distanceStar, main) VALUES (?, ?, ?, ?, ?, ?)", 
-                (id, system_id, name, systemNameAscii, distanceStar, 0))
+            c.execute("INSERT INTO stationIdMappings (stationId, systemId, stationName, systemName, distanceStar, starX, starY, starZ, main) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+                (id, system_id, name, systemNameAscii, distanceStar, system["x"], system["y"], system["z"], 0))
             
         marketCount += 1
     
