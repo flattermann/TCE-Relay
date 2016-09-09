924fb6d942534081b89b12b9b8c9154f6694c439
Christian Erpelding <flattermann@users.noreply.github.com>
2016-09-09 13:43:33 +0200

    Renamed import script

--- servertools/eddb-import/doEddbImport.sh
+++ servertools/eddb-import/doEddbImport.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /opt/eddb-import
 rm listings.csv
-wget -nv https://eddb.io/archive/v4/listings.csv && python eddb-import.py
+wget -nv https://eddb.io/archive/v4/listings.csv && python eddb-import-prices.py
